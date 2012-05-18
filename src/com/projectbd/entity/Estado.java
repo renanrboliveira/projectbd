5bbd87eadd7be3db6c54811dd5c8363c390e5134
paulo-neto <paulo.neto@dce.ufpb.br>
2012-05-18 20:17:28 -0300

    ---

--- src/com/projectbd/entity/Estado.java
+++ src/com/projectbd/entity/Estado.java
@@ -14,7 +14,7 @@ public class Estado implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	
 	@Column(nullable = false)
